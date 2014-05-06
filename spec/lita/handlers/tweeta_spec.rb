3da8013ae2a1a98466d33284ba2e0a5ff2a74969
Tristan Chong <tristanchong@gmail.com>
2014-05-06 13:28:17 -0700

    test for route and reply match

--- spec/lita/handlers/tweeta_spec.rb
+++ spec/lita/handlers/tweeta_spec.rb
@@ -1,4 +1,12 @@
 require "spec_helper"
 
 describe Lita::Handlers::Tweeta, lita_handler: true do
+  it { routes_command("hey jimmy https://twitter.com/tristaneuan/status/463007122087616512 check out this tweet").to(:tweeta) }
+
+  describe "#tweeta" do
+    it "returns the author and text of a linked tweet" do
+      send_command "hey jimmy https://twitter.com/tristaneuan/status/463007122087616512 check out this tweet"
+      expect(replies.first).to match "Tristan Chong said: Congratulations on 400 GitHub stars, @litachatbot and @jimmycuadra! #chatops #ruby https://www.lita.io/"
+    end
+  end
 end
